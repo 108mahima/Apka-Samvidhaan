77a2aa7234885bee1479e443c72d10762e400e79
nitesh815 <niteshsrivastava801@gmail.com>
2024-10-17 15:13:34 +0530

    color changes

--- src/components/home/HAbout.jsx
+++ src/components/home/HAbout.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import OnlineCourses from "../allcourses/OnlineCourses"
-import Heading from "../common/heading/Heading"
+//import Heading from "../common/heading/Heading"
 import "../allcourses/courses.css"
-import { coursesCard } from "../../dummydata"
+//import { coursesCard } from "../../dummydata"
 
 const HAbout = () => {
   return (
